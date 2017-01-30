14d162d8cd6ba19c095c48267a98f3a4ebe1226d
boyswan <jackwilliamboyce@gmail.com>
2017-01-30 08:49:18 +0000

    build

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -72,7 +72,7 @@ const filterResults = last((budgetInput, cart) => pipe(
   )
 ))
 
-const NoItems = () =>
+const noItems = () =>
 <NoResults color={Const.color.primary}>
   <MascotSad color={Const.color.primary}/>
   <h1>{Const.text.search.noResultsTitle}</h1>
@@ -86,6 +86,6 @@ export default connect(({
 }) =>
   <Grid>
     {/* {noItems()} */}
-    {searchResults ? filterResults(budgetInput, cart, searchResults) : NoItems()}
+    {searchResults ? filterResults(budgetInput, cart, searchResults) : noItems()}
   </Grid>
 )
