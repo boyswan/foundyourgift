b9357004d2ab4e633c462c66e2cb764291cbfac9
boyswan <jackwilliamboyce@gmail.com>
2017-02-02 23:07:19 +0000

    fix lowercase folders

---
+++ src/elements/Button/index.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from 'styled-components';
+import Const from 'utils/constants';
+import { prop } from 'ramda';
+
+const Button = styled.button`
+  transition: all 0.2s ease-in-out;
+  display: inline-block;
+  padding: 1.5rem 2rem;
+  border: 1px solid white;
+  font-size: 1.8rem;
+  cursor: pointer;
+  border-radius: 10rem;
+  font-weight: 100;
+  margin: 0 1rem 2rem 1rem;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+  }
+`;
+
+const small = () => `
+  padding: 1rem 1rem;
+  font-size: 1.4rem;
+  margin: 0 1rem 1rem 0rem;
+`;
+
+const primary = styled(Button)`
+  background: transparent;
+  color: white;
+  &:hover {
+    background: rgba(255, 255, 255, 0.5);
+    color: white;
+  }
+`;
+const selected = styled(Button)`
+  background: white;
+  color: red;
+`;
+const primaryLight = styled(primary)`
+  color: white;
+  background: ${prop('primary')};
+  /*&:hover {
+    color: ${prop('primary')};
+    background: rgba(255,255,255, 0.5);
+  }*/
+`;
+const selectedSmall = styled(selected)`
+  ${small()}
+`;
+const primarySmall = styled(primary)`
+  ${small()}
+`;
+const selectedSmallLight = styled(selected)`
+  ${small()}
+  color: white;
+  background: ${prop('primary')}
+`;
+const primarySmallLight = styled(primary)`
+  ${small()}
+  color: ${prop('secondary')};
+  background: white;
+  &:hover {
+    color: ${prop('primary')};
+    background: rgba(255,255,255, 0.5);
+  }
+`;
+
+export default ({ style = 'primary', onClick, label }) => {
+  const Button = ({
+    primary,
+    selected,
+    primaryLight,
+    selectedSmall,
+    primarySmall,
+    selectedSmallLight,
+    primarySmallLight
+  })[style];
+
+  return (
+    <Button primary={Const.color.primary} secondary={Const.color.secondary} onClick={onClick}>
+      {label}
+    </Button>
+  );
+};
---
+++ src/elements/Input/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import Actions from 'actions';
+import { Container, Label } from 'styles';
+import { Dropdown } from 'svg';
+
+const InputContainer = styled.div`
+  position: relative;
+  border-radius: 0.3rem;
+  background: white;
+  &:hover {
+    box-shadow: 0 1px 0 rgba(0, 0, 0, 0.06);
+  }
+`;
+const Input = styled.input`
+  transition: all 0.2s ease-in-out;
+  display: inline-block;
+  padding: 1rem;
+  border: none;
+  width: 100%;
+  border-radius: 0.3rem 0 0 0.3rem ;
+  font-weight: 300;
+  font-size: 1.4rem;
+  color: #4875A9;
+  width: ${({ icon }) => icon ? '100%' : '85%'};
+`;
+
+const Icon = styled.div`
+  display: inline-block;
+  position: absolute;
+  cursor: pointer;
+  right: 1rem;
+  transform: translatey(-50%);
+  top: 50%;
+`;
+
+const handleInput = (item, value) => Actions.setInput({ item, value });
+
+export default ({ item, value, label, icon, onClick }) => (
+  <Container>
+    <Label>{label}</Label>
+    <InputContainer>
+      <Input value={value} onChange={e => handleInput(item, e.target.value)} />
+      {icon ? <Icon onClick={onClick}>{icon}</Icon> : ''}
+    </InputContainer>
+  </Container>
+);
